03da0d80e8f5b7ab5cbee99a332f1614297e8406
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 09:26:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,39 @@ module "resource_groups" {
   for_each = var.regions
 
   name     = "rg-${each.key}-prod"
+  location = each.value.location
+}
+
+module "network" {
+
+  source = "./modules/network"
+
+  for_each = var.regions
+
+  resource_group_name = module.resource_groups[each.key].name
+
+  location            = each.value.location
+
+  vnet_name           = "vnet-${each.key}"
+
+  vnet_cidr           = each.value.vnet_cidr
+
+  subnet_name         = "aks-subnet"
+
+  subnet_cidr         = each.value.subnet_cidr
+}
+
+module "aks" {
+
+  source = "./modules/aks"
+
+  for_each = var.regions
+
+  cluster_name = "aks-${each.key}-prod"
 
   location = each.value.location
 
+  resource_group_name = module.resource_groups[each.key].name
+
+  subnet_id = module.network[each.key].subnet_id
 }
