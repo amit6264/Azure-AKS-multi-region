a4524f59d098b5e665bc6398bcecc695a2933450
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 09:55:25 +0530

    Create variables.tf

---
+++ modules/acr/variables.tf
@@ -0,0 +1,24 @@
+variable "acr_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "private_endpoint_subnet_id" {
+  type = string
+}
+
+variable "vnet_id" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
