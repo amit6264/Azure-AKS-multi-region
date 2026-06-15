efd203cf5f48172b2ea4a2f980e388052e4de67b
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 09:20:30 +0530

    Create main.tf

---
+++ modules/network/main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_virtual_network" "this" {
+
+  name                = var.vnet_name
+  address_space       = [var.vnet_cidr]
+
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+}
+
+resource "azurerm_subnet" "this" {
+
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+
+  virtual_network_name = azurerm_virtual_network.this.name
+
+  address_prefixes     = [var.subnet_cidr]
+
+}
