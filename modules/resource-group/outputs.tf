52a6f73bc558f74b1af32f432ad98cfb514c32d1
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 09:47:35 +0530

    Update outputs.tf

--- modules/resource-group/outputs.tf
+++ modules/resource-group/outputs.tf
@@ -1,7 +1,17 @@
+output "id" {
+  description = "Resource Group ID"
+
+  value = azurerm_resource_group.this.id
+}
+
 output "name" {
+  description = "Resource Group Name"
+
   value = azurerm_resource_group.this.name
 }
 
 output "location" {
+  description = "Resource Group Location"
+
   value = azurerm_resource_group.this.location
 }
