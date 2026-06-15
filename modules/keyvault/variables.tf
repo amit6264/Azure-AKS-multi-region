cd3d25b410a7839c2196aae52799a657a346e1cc
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 10:04:11 +0530

    Create variables.tf

---
+++ modules/keyvault/variables.tf
@@ -0,0 +1,28 @@
+variable "keyvault_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "private_endpoint_subnet_id" {
+  type = string
+}
+
+variable "vnet_id" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
