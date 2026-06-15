4387a8c53104ea06e928d97d6a10b82a72c365b1
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 10:09:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,3 +123,12 @@ module "keyvault" {
     ManagedBy   = "Terraform"
   }
 }
+
+resource "azurerm_role_assignment" "kv_secrets_user" {
+
+  scope = module.keyvault.id
+
+  role_definition_name = "Key Vault Secrets User"
+
+  principal_id = "<managed-identity-object-id>"
+}
