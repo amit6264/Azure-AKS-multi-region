4815928ed319f36644912ea2c476de53b91aa0d9
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 09:46:46 +0530

    Update variables.tf

--- modules/resource-group/variables.tf
+++ modules/resource-group/variables.tf
@@ -1,2 +1,17 @@
-variable "name" {}
-variable "location" {}
+variable "name" {
+  description = "Resource Group Name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+}
+
+variable "tags" {
+  description = "Common Tags"
+
+  type = map(string)
+
+  default = {}
+}
