d7470ce9bb890a91ad74d912ed27fd01a9b0f6a2
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 10:12:07 +0530

    Update variables.tf

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -1,5 +1,28 @@
-variable "cluster_name" {}
-variable "location" {}
+variable "cluster_name" {
+  type = string
+}
 
-variable "resource_group_name" {}
-variable "subnet_id" {}
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "dns_prefix" {
+  type = string
+}
+
+variable "aks_subnet_id" {
+  type = string
+}
+
+variable "log_analytics_workspace_id" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
